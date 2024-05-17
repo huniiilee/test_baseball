7e5df3d8412f8b105dcbc2585b79bfa85a0b6978
dhonghun.lee <dhonghun.lee@samsung.com>
2024-05-17 15:02:12 +0900

    [feature] calc strikes and balls

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -27,5 +27,20 @@ TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
 	EXPECT_TRUE(result.solved);
 	EXPECT_EQ(3, result.strikes);
 	EXPECT_EQ(0, result.balls);
+}
 
+TEST_F(BaseballFixture, ReturnSolvedResult2S0B) {
+	GuessResult result = game.guess("124");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(2, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResult1S2B) {
+	GuessResult result = game.guess("132");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(2, result.balls);
 }
--- cra3-0517-baseball/baseball.cpp
+++ cra3-0517-baseball/baseball.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <unordered_map>
 
 using namespace std;
 
@@ -18,7 +19,7 @@ public:
 			|| guessNumber[0] == guessNumber[2]
 			|| guessNumber[1] == guessNumber[2];
 	}
-	
+
 	void assertIllegalArgument(const string& guessNumber) {
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
@@ -29,8 +30,40 @@ public:
 			throw invalid_argument("Must be number");
 		}
 
-		if(isDuplicatedNumber(guessNumber))
+		if (isDuplicatedNumber(guessNumber))
 			throw invalid_argument("Must not have the same number");
+	}
+
+	int countStrike(const std::string& guessNumber) {
+		int strikes = 0;
+		for (size_t i = 0; i < question.length(); ++i) {
+			if (question[i] == guessNumber[i]) {
+				++strikes;
+			}
+		}
+		return strikes;
+	}
+	
+
+	int countBall(const std::string& guessNumber) {
+		unordered_map<char, int> questionMap;
+		unordered_map<char, int> guessMap;
+
+		for (size_t i = 0; i < question.length(); ++i) {
+			if (question[i] == guessNumber[i]) continue;
+			questionMap[question[i]]++;
+			guessMap[guessNumber[i]]++;
+		}
+
+		int balls = 0;
+		for (const auto& entry : guessMap) {
+			char ch = entry.first;
+			int countInGuess = entry.second;
+			int countInQuestion = questionMap[ch];
+			balls += min(countInGuess, countInQuestion);
+		}
+
+		return balls;
 	}
 
 	GuessResult guess(const string& guessNumber) {
@@ -38,7 +71,9 @@ public:
 		if (guessNumber == question) {
 			return { true, 3, 0 };
 		}
-		return { false,0, 0 };
+		else {
+			return { false, countStrike(guessNumber), countBall(guessNumber) };
+		}
 	}
 
 private:
