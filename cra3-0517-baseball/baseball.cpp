40dab408ce7c45f54336a1e6b090aa8f751373ef
dhonghun.lee <dhonghun.lee@samsung.com>
2024-05-17 14:20:28 +0900

    [refactor] page 51

--- cra3-0517-baseball/baseball.cpp
+++ cra3-0517-baseball/baseball.cpp
@@ -2,15 +2,24 @@
 
 using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
+	explicit Baseball(const string& question) : question(question) {
+	}
+
 	bool isDuplicatedNumber(const string& guessNumber) {
 		return guessNumber[0] == guessNumber[1]
 			|| guessNumber[0] == guessNumber[2]
 			|| guessNumber[1] == guessNumber[2];
 	}
 	
-	void guess(const string& guessNumber) {
+	void assertIllegalArgument(const string& guessNumber) {
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
@@ -22,8 +31,17 @@ public:
 
 		if(isDuplicatedNumber(guessNumber))
 			throw invalid_argument("Must not have the same number");
-	
 	}
 
+	GuessResult guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+		if (guessNumber == question) {
+			return { true, 3, 0 };
+		}
+		return { false,0, 0 };
+	}
+
+private:
+	string question;
 
 };
